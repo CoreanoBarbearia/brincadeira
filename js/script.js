dc614f9cdd52d4453f45bcef2ba6ae93a95daf3d
Guilherme Clara de Carvalho <143226753+CoreanoBarbearia@users.noreply.github.com>
2023-11-15 12:49:20 -0300

    a

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('n').addEventListener('mouseenter' , () =>{
+    var numA = Math.floor(Math.random() * 300) 
+    document.getElementById('n').style.left= numA +'px'
+    document.getElementById('n').style.top= numA +'px'
+})
+
+document.getElementById('s').addEventListener('click' , () =>{
+    alert('allaaaaaa é gay')
+})
+
